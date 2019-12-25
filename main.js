52a9faf6a59fec495cff6cea8dc2c4641a3d0b00
orthrin <gronzelg@gmail.com>
2019-12-26 01:57:30 +0300

    source 1-3

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
-app.on('ready', () =>{
+app.on('ready', () =>
+{
 
+    // ==== Instantiation
     mainWindow = new BrowserWindow({});
 
+    // >> Create main window
     mainWindow.loadURL(
         url.format({
             pathname: path.join(__dirname, "project/view-html/index.html"),
@@ -17,5 +20,52 @@ app.on('ready', () =>{
             slashes: true
         })
     );
-    
-})
+
+    console.log(process.platform);
+
+    // ==== Create menu
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+
+});
+
+const mainMenuTemplate = [
+    {
+        label: "File",
+        submenu: [
+            {
+                label: "Add"
+            },
+            {
+                label: "Delete"
+            },
+            {
+                label: "Quit",
+                accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",        // key 
+                role: "quit"
+            }
+        ]
+    },
+    {
+        label: "Reload",
+        role: "reload"
+    }
+]
+
+// >> Eger platform mac ise 
+if (process.platform == "darwin")
+{
+
+}
+
+if (process.env.NODE_ENV !== "production")
+{
+    mainMenuTemplate.push({
+        label: "DevTools",
+        click(item, focusedWindow)
+        {
+            focusedWindow.toggleDevTools();
+        }
+    })
+}
--- notebook.md
+++ notebook.md
@@ -5,9 +5,17 @@
 # Problems
 
 # Reminders
+>> Comment Nomenclature
+- <prefix> ==== Title
+- <prefix> >> Function What is it doing
+- <prefix> General Purpose Comment
+
 >> Source
 Kablosuz kedi masaustu uyguulama yapimi
 https://www.youtube.com/watch?v=qsM9ylyt7xw&list=PL_f2F0Oyaj48jgl98pHuoyxoTgi7gXJ-z
+
+>> Javascript
+`console.log(process.platform)` platform bilgisi
 
 >> Assume Unchanged
 
