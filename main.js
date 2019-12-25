f042ed06b83a05bb78db58f268f0632101cbcfc0
orthrin <gronzelg@gmail.com>
2019-12-26 01:27:43 +0300

    first source 2

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+app.on('ready', () =>{
+
+    mainWindow = new BrowserWindow({});
+
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "project/view-html/index.html"),
+            protocol: "file:",
+            slashes: true
+        })
+    );
+    
+})
